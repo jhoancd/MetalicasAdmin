7a24699d536bca5b0519e9dbbff0f5eaa8faacc1
jhoancd <jhoancd99@gmail.com>
2024-03-04 21:25:32 -0500

    update

--- client/src/components/dashboard/modal/ModalRegistarVenta.jsx
+++ client/src/components/dashboard/modal/ModalRegistarVenta.jsx
@@ -6,57 +6,57 @@ import { useEffect, useState } from "react";
 import { sum, moneda, hoy } from "../tools"
 import Axios from 'axios';
 import toast, { Toaster } from 'react-hot-toast';
-import {url} from "../var.js"
-
+import { url } from "../var.js"
+import Select from "react-select";
 
 
 export default function ModalRegistrarVenta({ modalVenta, toggle, agregarVenta }) {
 
-  const notifyError = (msg) => toast.error(msg);
-  const [inventario, setInventario] = useState([])
-
-
-
-    const { register, handleSubmit, watch } = useForm({
+    const notifyError = (msg) => toast.error(msg);
+    const [inventario, setInventario] = useState([])
+    const [listaItems, setListaItems] = useState([])
+    const [selectItem, setSelectItem] = useState("")
+    const { register, handleSubmit, watch, reset } = useForm({
         defaultValues: {
             fecha: hoy(),
-            items: {
-                item1: {
-                    precio: 0,
-                    cantidad: 0
-                },
-                item2: {
-                    precio: 0,
-                    cantidad: 0
-                },
-                item3: {
-                    precio: 0,
-                    cantidad: 0
-                },
-                item4: {
-                    precio: 0,
-                    cantidad: 0
-                },
-                item5: {
-                    precio: 0,
-                    cantidad: 0
-                }
-            }
-
+            precio: 0,
+            cantidad: 0
         }
     })
 
+    const eliminar = itemId => {
+        const filtredData = listaItems.filter(item => item.id !== itemId);
+        setListaItems(filtredData);
+    };
+
+    // Obtener el valor del select y almacenarlo
+    const handelSelectChange = (e) => {
+        setSelectItem(e)
+    }
+
+    // Funcion para agregar el articulo al codigo
+    const agregarArticulo = () => {
+        const item = {
+            "id": selectItem.value,
+            "cantidad": watch("cantidad"),
+            "articulo": selectItem.label,
+            "precio": watch("precio")
+        }
+        setListaItems([...listaItems, item])
+    }
+
     const data = watch()
+
     const pago = watch("abono")
 
-    const obtenerInventario = () =>{
-            Axios.get(`${url}/obtenerInventario`).then((res) => {
-              setInventario(res.data)
-            }).catch((err) => {
-              notifyError(`Error al obtener inventario: ${err}`);
-            })
+    const obtenerInventario = () => {
+        Axios.get(`${url}/obtenerInventario`).then((res) => {
+            setInventario(res.data)
+        }).catch((err) => {
+            notifyError(`Error al obtener inventario: ${err}`);
+        })
     }
-   
+
     const articulos = () => {
         return inventario.map((val) => {
             return (<option value={val.descripcion}>{val.descripcion}</option>)
@@ -78,25 +78,25 @@ export default function ModalRegistrarVenta({ modalVenta, toggle, agregarVenta }
     })
 
     return (<div>
-           <Toaster
-        position="top-center"
-        reverseOrder={false}
-        gutter={8}
-        containerClassName=""
-        containerStyle={{}}
-        toastOptions={{
-          // Define default options
-          className: '',
-          duration: 5000,
-          success: {
-            duration: 3000,
-            theme: {
-              primary: 'green',
-              secondary: 'black',
-            },
-          },
-        }}
-      />
+        <Toaster
+            position="top-center"
+            reverseOrder={false}
+            gutter={8}
+            containerClassName=""
+            containerStyle={{}}
+            toastOptions={{
+                // Define default options
+                className: '',
+                duration: 5000,
+                success: {
+                    duration: 3000,
+                    theme: {
+                        primary: 'green',
+                        secondary: 'black',
+                    },
+                },
+            }}
+        />
 
         <Modal isOpen={modalVenta} toggle={toggle} fullscreen="sm" size="lg" scrollable={true} animation="false">
             <ModalHeader toggle={toggle}><i className="bi bi-cart-plus"> </i>Registrar venta</ModalHeader>
@@ -126,6 +126,38 @@ export default function ModalRegistrarVenta({ modalVenta, toggle, agregarVenta }
                     </FormGroup>
                     <p>Articulos</p>
                     <div className="tabla-articulos">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-3">
+                                    <input className="form-control"
+                                        type="number"
+                                        {...register("cantidad")}
+                                    />
+                                </div>
+                                <div className="col-6">
+                                    <Select
+                                        defaultValue={{ label: "Seleccione un articulo", value: "Default" }}
+                                        options={inventario.map(val => {
+                                            return ({ label: val.descripcion, value: val.id })
+                                        }
+                                        )}
+                                        onChange={handelSelectChange}
+                                    />
+                                </div>
+                                <div className="col-3">
+                                    <input className="form-control"
+                                        type="number"
+                                        {...register("precio")}
+                                    />
+                                </div>
+                            </div>
+                            <div className="row mt-2">
+                                <button
+                                    className="btn btn-primary btn-large"
+                                    onClick={() => { reset() }}
+                                > Agregar</button>
+                            </div>
+                        </div>
 
                         {/* TABLA DE ARTICULOS FACTURA */}
                         <Table
@@ -137,127 +169,39 @@ export default function ModalRegistrarVenta({ modalVenta, toggle, agregarVenta }
                                     <th style={{ width: "10%" }}>Cant.</th>
                                     <th style={{ width: "50%" }}>Articulo </th>
                                     <th style={{ width: "10%" }}>Precio</th>
+                                    <th style={{ width: "10%" }}>Total</th>
+                                    <th style={{ width: "10%" }}>Del</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>
-                                        <input className="form-control"
-                                            {...register("items.item1.cantidad", { valueAsNumber: true })}
-                                            type="number"
-                                        />
-                                    </td>
-                                    <td>
-                                        <select input className="form-control"
-                                            {...register("items.item1.articulo")}
-                                            type="select"
-                                        >
-                                            <option value=""></option>
-                                            {articulos()}
-                                        </select>
-                                    </td>
-                                    <td>
-                                        <input className="form-control"
-                                            {...register("items.item1.precio", { valueAsNumber: true })}
-                                            type="number"
-                                        />
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <input className="form-control"
-                                            {...register("items.item2.cantidad", { valueAsNumber: true })}
-                                            type="number"
-                                        />
-                                    </td>
-                                    <td>
-                                        <select input className="form-control"
-                                            {...register("items.item2.articulo")}
-                                            type="select"
-                                        >
-                                            <option value=""></option>
-                                            {articulos()}
-                                        </select>
-                                    </td>
-                                    <td>
-                                        <input className="form-control"
-                                            {...register("items.item2.precio", { valueAsNumber: true })}
-                                            type="number"
-                                        />
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <input className="form-control"
-                                            {...register("items.item3.cantidad", { valueAsNumber: true })}
-                                            type="number"
-                                        />
-                                    </td>
-                                    <td>
-                                        <select input className="form-control"
-                                            {...register("items.item3.articulo")}
-                                            type="select"
-                                        >
-                                            <option value=""></option>
-                                            {articulos()}
-                                        </select>
-                                    </td>
-                                    <td>
-                                        <input className="form-control"
-                                            {...register("items.item3.precio", { valueAsNumber: true })}
-                                            type="number"
-                                        />
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <input className="form-control"
-                                            {...register("items.item4.cantidad", { valueAsNumber: true })}
-                                            type="number"
-                                        />
-                                    </td>
-                                    <td>
-                                        <select input className="form-control"
-                                            {...register("items.item4.articulo")}
-                                            type="select"
-                                        >
-                                            <option value=""></option>
-                                            {articulos()}
-                                        </select>
-                                    </td>
-                                    <td>
-                                        <input className="form-control"
-                                            {...register("items.item4.precio", { valueAsNumber: true })}
-                                            type="number"
-                                        />
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <input className="form-control"
-                                            {...register("items.item5.cantidad", { valueAsNumber: true })}
-                                            type="number"
-                                        />
-                                    </td>
-                                    <td>
-                                        <select input className="form-control"
-                                            {...register("items.item5.articulo")}
-                                            type="select"
-                                        >
-                                            <option value=""></option>
-                                            {articulos()}
-                                        </select>
-                                    </td>
-                                    <td>
-                                        <input className="form-control"
-                                            {...register("items.item5.precio", { valueAsNumber: true })}
-                                            type="number"
-                                        />
-                                    </td>
-                                </tr>
+                                {
+                                    listaItems.map((val, key) => {
+                                        return <tr key={key}>
+                                            <td>
+                                                {val.cantidad}
+                                            </td>
+                                            <td>
+                                                {val.articulo}
+                                            </td>
+                                            <td>
+                                                {val.precio}
+                                            </td>
+                                            <td>
+                                                {val.cantidad * val.precio}
+                                            </td>
+                                            <td>
+                                                <button
+                                                    className="btn btn-danger"
+                                                    onClick={() => eliminar(val.id)}
+                                                > - </button>
+                                            </td>
+                                        </tr>
+                                    })
+                                }
+
                             </tbody>
                         </Table>
-                        <div className="mb-2 fs-5 text-end"><b>Total: </b> {moneda(sum(data))}</div>
+                        <div className="mb-2 fs-5 text-end"><b>Total: </b> </div>
 
                         {/*FIN TABLA DE ARTICULOS FACTURA */}
 
@@ -299,7 +243,7 @@ export default function ModalRegistrarVenta({ modalVenta, toggle, agregarVenta }
 
             </ModalBody>
             <ModalFooter>
-                <span style={{ padding: "0 4px" }} className={`rounded-pill ${sum(data) <= pago ? "text-bg-success" : "text-bg-warning"}`}><i className={`bi ${sum(data) <= pago ? "bi-check-circle" : "bi-exclamation-circle"}`}> </i></span>
+                <span style={{ padding: "0 4px" }} className={`rounded-pill ${/*sum(data) <= pago*/ true ? "text-bg-success" : "text-bg-warning"}`}><i className={`bi ${true ? "bi-check-circle" : "bi-exclamation-circle"}`}> </i></span>
                 <Button color="outline-danger" onClick={toggle}>
                     <i className="bi bi-x"> </i> Cancelar
                 </Button>
